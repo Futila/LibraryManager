936c964c9f6a9395daef25ed83611a79e5b60aa0
Fernando Futila <fernando.futila@gmail.com>
2024-08-12 17:14:29 -0300

    docs: criação do readme

--- obj/Debug/net8.0/LibraryManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/LibraryManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c204afa798b8a6775799d237ec38921af4c16b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ readme.md
@@ -0,0 +1,34 @@
+# LibraryManager API
+
+The main objective is to develop an online bookstore API that allows users to create, view, edit and delete books.
+
+<!-- image -->
+
+<div align="center">
+    <img src="./images/swagger.png"  />
+</div>
+
+## Features
+
+- [x] Create Book
+- [x] View Book
+- [x] Edit Book
+- [x] Delete Book
+
+## What was used ?
+
+The project was created on react
+
+- [x] C#
+- [x] .NET Core
+- [x] Visual Studio versão 2022
+
+## Meta
+
+Author - Rockeseat.
+
+programmer - [Fernando Futila] - fernando.futila@gmail.com
+
+---
+
+Done with 💜 by Fernando Futila 👋🏻
